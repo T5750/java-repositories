e3ee89f965a029a00c3b055bc0ffc40aa8739114
T5750 <evangel_a@sina.com>
2018-11-02 11:12:28 +0800

    updated JVM.

---
+++ jvm/src/main/java/evangel/bjsxt/base001/Test04.java
@@ -0,0 +1,22 @@
+package evangel.bjsxt.base001;
+
+public class Test04 {
+	// -Xss1m
+	// -Xss5m
+	// 栈调用深度
+	private static int count;
+
+	public static void recursion() {
+		count++;
+		recursion();
+	}
+
+	public static void main(String[] args) {
+		try {
+			recursion();
+		} catch (Throwable t) {
+			System.out.println("调用最大深入：" + count);
+			t.printStackTrace();
+		}
+	}
+}
