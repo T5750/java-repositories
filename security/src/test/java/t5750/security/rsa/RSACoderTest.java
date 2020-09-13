0fd51868759c6cfcdc7061ae66b8a41644df76de
T5750 <evangel_a@sina.com>
2020-09-13 14:25:55 +0800

    Update junit

--- security/src/test/java/t5750/security/rsa/RSACoderTest.java
+++ security/src/test/java/t5750/security/rsa/RSACoderTest.java
@@ -9,6 +9,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.Before;
 import org.junit.Test;
+import t5750.security.rsa.RSACoder;
 
 /**
  * RSA数字签名校验
